67a34c270d20fa4c7eae1ade6ac249efe482ccfc
Leiiph <60736065+Leiiph@users.noreply.github.com>
2024-07-20 18:32:26 +0200

    front push

--- my-app/backend/connect.js
+++ my-app/backend/connect.js
@@ -1,11 +1,11 @@
 // File to handle connection with the mysql database
-
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'api-m1.clewiamiykdf.us-east-2.rds.amazonaws.com',
-    user: 'admin',
-    password: 'adminadmin',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     database:'projet_api'
 });
 
